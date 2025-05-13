0b206b145a53c4a21bd78670838487c1271b1c84
SilmaraAlmeida <silmaraalmeida226@gmail.com>
2025-05-13 09:26:08 -0300

    index modificado pra tratar as rotas

--- public/index.php
+++ public/index.php
@@ -1,15 +1,39 @@
 <?php
+require_once __DIR__ . '/../app/Helper/chamarView.php';
 
 use Core\Library\Ambiente;
 use Core\Library\Routes;
 
 require_once ".." . DIRECTORY_SEPARATOR . "vendor" . DIRECTORY_SEPARATOR . "autoload.php";
 require_once ".." . DIRECTORY_SEPARATOR . "app" . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "Constants.php";
+require_once ".." . DIRECTORY_SEPARATOR . "app" . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "Routes.php";
 
 $ambiente = new Ambiente();
 $routes = new Routes();
 
 $ambiente->load();
 
-// Chamando minha rota
-$routes->rota();
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$route = Routes::getControllerMethod($requestUri);
+
+if ($route) {
+    $controllerName = 'App\\Controller\\' . $route['controller'];
+    $methodName = $route['method'];
+
+    if (class_exists($controllerName)) {
+        $controller = new $controllerName();
+        if (method_exists($controller, $methodName)) {
+            $controller->$methodName();
+        } else {
+            echo "Método $methodName não encontrado no controller $controllerName.";
+        }
+    } else {
+        echo "Controller $controllerName não encontrado.";
+    }
+} else {
+    echo "Rota não encontrada!";
+}
+
+
+?>
